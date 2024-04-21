01caea2b36928058e4843487b7f4a88cf8a2aad8
Mitya Selivanov <automainint@guattari.tech>
2024-04-21 23:12:32 +0200

    Print different bytes

--- mapping_test.sh
+++ mapping_test.sh
@@ -1,12 +1,12 @@
 cargo test
-if cmp "mapping.txt" "mapping_ref.txt"; then
+if cmp -b "mapping.txt" "mapping_ref.txt"; then
   echo "OK"
 else
   echo "FAIL: indices don't match the reference"
   exit 1
 fi
 cargo test --release
-if cmp "mapping.txt" "mapping_ref.txt"; then
+if cmp -b "mapping.txt" "mapping_ref.txt"; then
   echo "OK"
 else
   echo "FAIL: indices don't match the reference"
