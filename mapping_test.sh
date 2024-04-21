2e6321461dcd9618f03d20cf371e4fe159488362
Mitya Selivanov <automainint@guattari.tech>
2024-04-21 22:33:53 +0200

    mapping determinism test

--- mapping_test.sh
+++ mapping_test.sh
@@ -2,14 +2,14 @@ cargo test
 if cmp -s "mapping.txt" "mapping_ref.txt"; then
   echo "OK"
 else
-  echo "FAIL"
+  echo "FAIL: indices don't match the reference"
   exit 1
 fi
 cargo test --release
 if cmp -s "mapping.txt" "mapping_ref.txt"; then
   echo "OK"
 else
-  echo "FAIL"
+  echo "FAIL: indices don't match the reference"
   exit 1
 fi
 exit 0
